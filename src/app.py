e148efd353d9a603daabcf9923299689d3a7f967
Chris Atkin <atkinchris@gmail.com>
2024-08-18 20:21:58 +0100

    Don't bind to all interfaces

--- src/app.py
+++ src/app.py
@@ -156,6 +156,7 @@ def create_transaction():
     # Create a new transaction
     pass
 
+
 # Only run the app if this file is the entry point
-if __name__=='__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(port=5000)
