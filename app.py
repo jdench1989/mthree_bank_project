70108c9c004ce89ccbe032a4f5219351cf968745
jdench1989 <jdench1989@gmail.com>
2024-08-19 13:50:33 +0100

    Implements user login endpoint

--- app.py
+++ app.py
@@ -51,6 +51,16 @@ def login():
     # Show the login form with message (if any)
     return render_template('index.html', msg=msg)
 
+    # http://localhost:5000/python/logout - this will be the logout page
+@app.route('/bank/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
+
 @app.route('/')
 def index():
     # Eventually will show login page and then show main menu for logged in user
