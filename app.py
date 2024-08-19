3b622af8fc1a80e53795417889cfa0224f51030f
jdench1989 <jdench1989@gmail.com>
2024-08-19 17:44:23 +0100

    login, logout and register fixed to work with mysql.connector and removed now defunct imports for flask-mysqldb as no longer in use

--- app.py
+++ app.py
@@ -1,24 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
-# from flask_mysqldb import MySQL
-# import MySQLdb.cursors
-# import MySQLdb.cursors, re, hashlib
 from tabulate import tabulate
 from db.database_connection import database_connection
 import hashlib
+import re
 
 app = Flask(__name__)
 
 # Change this to your secret key (it can be anything, it's for extra protection)
 app.secret_key = 'your secret key'
-
-# # Enter your database connection details below
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = ''
-# app.config['MYSQL_DB'] = 'bank'
-
-# # Intialize MySQL
-# mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -45,7 +34,7 @@ def login():
         account = cursor.fetchone()
         cursor.close()
         conn.close()
-        # If account exists in accounts table in out database
+        # If account exists in accounts table in our database
         print(account)
         if account:
             # Create session data, we can access this data in other routes
@@ -55,12 +44,12 @@ def login():
             # Redirect to home page
             return redirect(url_for('home'))
         else:
-            # Account doesnt exist or username/password incorrect
+            # Account doesn't exist or username/password incorrect
             msg = 'Incorrect username/password!'
     # Show the login form with message (if any)
     return render_template('index.html', msg=msg)
 
-    # http://localhost:5000/python/logout - this will be the logout page
+    # http://localhost:5000/bank/logout - this will be the logout page
 @app.route('/bank/logout')
 def logout():
     # Remove session data, this will log the user out
@@ -70,19 +59,19 @@ def logout():
    # Redirect to login page
    return redirect(url_for('login'))
 
-# http://localhost:5000/pythinlogin/register - this will be the registration page, we need to use both GET and POST requests
+# http://localhost:5000/bank/register - this will be the registration page, we need to use both GET and POST requests
 @app.route('/bank/register', methods=['GET', 'POST'])
 def register():
     # Output message if something goes wrong...
     msg = ''
     # Check if "username", "password" and "email" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        conn, cursor = database_connection()
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
         # Check if account exists using MySQL
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
         account = cursor.fetchone()
         # If account exists show error and validation checks
@@ -101,7 +90,7 @@ def register():
             password = hash.hexdigest()
             # Account doesn't exist, and the form data is valid, so insert the new account into the accounts table
             cursor.execute('INSERT INTO users VALUES (NULL, %s, %s, %s)', (username, password, email,))
-            mysql.connection.commit()
+            conn.commit()
             msg = 'You have successfully registered!'
     elif request.method == 'POST':
         # Form is empty... (no POST data)
